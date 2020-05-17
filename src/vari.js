68fdcd07dd3e8894444ff9498dc5618914625abc
varinea <varinea.lopez@gmail.com>
2020-05-17 02:35:58 -0400

    fixed button

--- src/vari.js
+++ src/vari.js
@@ -1,12 +1,1 @@
-//BOTON de ACCIÓN - VARINEA--
 
-document.getElementById('rootVari').addEventListener ('click', () => {	
-        html.innerHTML +=  `
-        <h1>formulario</h1>
-
-        
-        `;
-	});
-//  })  
-//     document.getElementById('todo').style.display='none';
-//  });
