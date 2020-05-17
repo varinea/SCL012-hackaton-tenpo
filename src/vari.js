86cd2fcbf15b70499ad546e2e9d4d30b9a8e7590
varinea <varinea.lopez@gmail.com>
2020-05-16 23:01:21 -0400

    HtmlDinamico

---
+++ src/vari.js
@@ -0,0 +1,12 @@
+//BOTON de ACCIÓN - VARINEA--
+
+document.getElementById('rootVari').addEventListener ('click', () => {	
+        html.innerHTML +=  `
+        <h1>formulario</h1>
+
+        
+        `;
+	});
+//  })  
+//     document.getElementById('todo').style.display='none';
+//  });
