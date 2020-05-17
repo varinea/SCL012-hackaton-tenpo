2f2452477ce559357559823a9972da6c1c4056a0
SusejR <rodriguezbsusej@gmail.com>
2020-05-17 02:34:19 -0400

    vistas feedback lista

--- src/app.js
+++ src/app.js
@@ -1,24 +1,59 @@
 const root = document.getElementById("root");
-const boton = document.getElementById("btnFeedback");
+const btnFeedback = document.getElementById("btnFeedback");
 const h1Header = document.getElementById("h1Header");
+const btnClient = document.getElementById("btnClient");
 
-const cambiaRoot = () => {
+
+const feedback = () => {
+  h1Header.innerHTML=`Feedback`
+  root.innerHTML=`
+  <button class="buttons" id="feedbackForm"><strong>Formulario Feedback</strong></button>
+  <button class="buttons" id="feedbackState"><strong>Estado Feedback</strong></button>
+  `
+  const feedbackForm = document.getElementById("feedbackForm");
+  feedbackForm.addEventListener("click", form);
+
+}
+
+const form = () => {
   h1Header.innerHTML=`Formulario Feedback`
   root.innerHTML=`<div class="col s12">
   <p>Información Personal</p>
-   <div class="divider"></div>
+   
     <div class="seccion">
     <textarea class="text" placeholder="Nombre"></textarea>
     <textarea class="text" placeholder="Apellido"></textarea>
     <textarea class="text" placeholder="Rut" ></textarea>
     <textarea class="text" placeholder="E-mail" ></textarea>
     <textarea class="text" placeholder="Telefono Contacto"></textarea>   
-    <button type="button" height="42" width="42"><a class="waves-effect waves-light btn"><i class="material-icons left">Continuar</i></a></button>   
+    <button type="button" ><a class="waves-effect waves-light btn">Continuar</a></button>   
    </div>
   </div>`
+
 }
-boton.addEventListener("click", cambiaRoot);
 
 
 
+const clientAtention = () => {
+  h1Header.innerHTML=`Atención al Cliente`
+  root.innerHTML=`<div class="caja">
+  <img class="img" src="./img/mensaje.jpeg">
+  <div class="text">
+  <h5>Te ayudamos</h5>
+  <a href="https://wa.me/56958048564" class="waves-effect waves-light btn"><i class="fab fa-whatsapp"></i> WHATSAPP SOPORTE</a>
+  <p><strong>Horario de Atención: <strong>Lunes a Viernes 08:00-23:00 hrs. Sábados, Domingos y Festivos 09:00-23:00 hrs.</p></div>
+  </div>
+  <div class="caja">
+  <img class="img" src="./img/fono.jpg">
+  <div class="text">
+  <h5>¿Tienes otra duda?</h5>
+  <h6>Llámanos al 600 360 0355</h6>
+  <p><strong>Horario de Atención: <strong>Lunes a Viernes 08:00-23:00 hrs. Sábados, Domingos y Festivos 09:00-23:00 hrs.</p></div>
+  </div>  
+  `
+}
+
+btnFeedback.addEventListener("click", feedback);
+
+btnClient.addEventListener("click", clientAtention);
 
